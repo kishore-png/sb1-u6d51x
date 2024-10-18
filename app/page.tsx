69c251d69ab0ae0f0c561aab757785b4f5ac009b
kishore-png <kishore.m@capestart.com>
2024-10-19 03:08:24 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,19 +3,24 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <h1 className={styles.title}>Lae sam!!</h1>
-      <div className={styles.playerWrapper}>
-        <Image
-          src={
-            'https://res.cloudinary.com/dtid3x3aq/image/upload/IMG_20241019_030318305_bsmrjz.jpg'
-          }
-          alt={''}
-          width={270}
-          height={1080}
-          style={{ objectFit: 'cover' }}
-        />
-      </div>
-    </main>
+    <iframe
+      src="https://www.youtube.com/watch?v=gfU1iZnjRZM"
+      frameborder="0"
+      allowfullscreen
+    />
+    // <main className={styles.main}>
+    //   <h1 className={styles.title}>Lae sam!!</h1>
+    //   <div className={styles.playerWrapper}>
+    //     <Image
+    //       src={
+    //         'https://res.cloudinary.com/dtid3x3aq/image/upload/IMG_20241019_030318305_bsmrjz.jpg'
+    //       }
+    //       alt={''}
+    //       width={270}
+    //       height={1080}
+    //       style={{ objectFit: 'cover' }}
+    //     />
+    //   </div>
+    // </main>
   );
 }
